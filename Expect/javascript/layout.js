71af954404c2cdbb1d9ad3c4992bed85636b72b5
Gary <163843362+garyfan1204@users.noreply.github.com>
2024-09-24 21:17:54 +0800

    Add files via upload

---
+++ Expect/javascript/layout.js
@@ -0,0 +1,76 @@
+const pages = {
+    "/index.html": "index.html",
+    "/userinfo.html": "userinfo.html",
+    "/dummie.html": "dummie.html",
+    "/test.html": "test.html",
+    "/result.html": "result.html"
+}
+
+const titles = {
+    "index.html": "職得期待",
+    "userinfo.html": "用戶資料",
+    "dummie.html": "資訊懶人包",
+    "test.html": "職涯測驗",
+    "result.html": "測驗結果"
+}
+
+const cssFiles = {
+    "index.html": ["css/layout.css", "css/index.css"],
+    "userinfo.html": ["css/layout.css", "css/userinfo.css"],
+    "dummie.html": ["css/layout.css", "css/dummie.css"],
+    "test.html": ["css/layout.css", "css/test.css"],
+    "result.html": ["css/layout.css", "css/result.css"]
+}
+
+const jsFiles = {
+    "index.html": ["javascript/carousel.js"],
+    "test.html":  ["javascript/test.js"]
+}
+
+function layout(page) {
+    fetch("layout.html")
+    .then((response) => response.text())
+    .then((html) => {
+        document.body.innerHTML = html;
+        return fetch(page);
+    })
+    .then((response) => response.text())
+    .then((pageHtml) => {
+        const main = document.querySelector("main");
+        main.innerHTML = pageHtml;
+        const title = titles[page];
+        const cssFile = cssFiles[page];
+        const jsFile = jsFiles[page];
+
+        if (title) {
+            document.title = title;
+        }
+        
+        if (cssFile) {
+            cssFile.forEach((cssfile) => {
+                const link = document.createElement("link");
+                link.rel = "stylesheet";
+                link.href = cssfile;
+                document.head.appendChild(link);
+            });
+        }
+
+        if (jsFile) {
+            jsFile.forEach((jsfile) => {
+                const script = document.createElement("script");
+                script.src = jsfile;
+                script.async = false;
+                document.body.appendChild(script);
+            });
+        }
+    })
+    .catch((error) => {
+        console.error("加載指定頁面失敗：", error);
+    });
+}
+
+window.addEventListener("DOMContentLoaded", function () {
+    const pathname = window.location.pathname;
+    let page = pages[pathname] || "index.html";
+    layout(page);
+});
